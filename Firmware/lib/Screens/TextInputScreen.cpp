9a4598adcc26cd2376f562a8eeb0a796f03fd58e
Daniel Grießhaber <dangrie158@gmail.com>
2016-07-30 01:01:09 +0200

    added a Text Input Screen

---
+++ Firmware/lib/Screens/TextInputScreen.cpp
@@ -0,0 +1,40 @@
+#include "TextInputScreen.h"
+
+TextInputScreen::TextInputScreen(SSD1306 *display, const String message)
+    : Screen(display), mMessage(message), mInput((char)START_CHAR),
+      mCurrentCharPos(0) {}
+
+TextInputScreen::~TextInputScreen() {}
+
+void TextInputScreen::draw() {
+  this->clear();
+  this->mDisplay->setColor(WHITE);
+  this->mDisplay->setFont(ArialMT_Plain_10);
+  this->mDisplay->setTextAlignment(TEXT_ALIGN_CENTER);
+
+  this->mDisplay->drawString(SCREEN_WIDTH / 2, (SCREEN_HEIGHT / 2) + 20,
+                             mMessage);
+
+  // use a progressbar with 0 progress to easily draw a nice frame
+  this->mDisplay->drawProgressBar(0, SCREEN_HEIGHT / 2, SCREEN_WIDTH - 1, 16,
+                                  0);
+
+  // draw the current input
+  this->mDisplay->setTextAlignment(TEXT_ALIGN_LEFT);
+  int inputStartX = 8;
+
+  this->mDisplay->drawString(inputStartX, (SCREEN_HEIGHT / 2) + 1, mInput);
+
+  // draw the (blinking) cursor (0.5Hz)
+  if ((millis() % 1000) > 500) {
+    this->mDisplay->setColor(INVERSE);
+    String inputToStartCurrentChar = mInput.substring(0, mCurrentCharPos);
+    String inputToEndCurrentChar = mInput.substring(0, mCurrentCharPos + 1);
+    int cursorStartX =
+        inputStartX + this->mDisplay->getStringWidth(inputToStartCurrentChar);
+    int cursorEndX =
+        inputStartX + this->mDisplay->getStringWidth(inputToEndCurrentChar);
+    this->mDisplay->fillRect(cursorStartX, (SCREEN_HEIGHT / 2) + 3,
+                             cursorEndX - cursorStartX, 12);
+  }
+}
---
+++ Firmware/lib/Screens/TextInputScreen.h
@@ -0,0 +1,59 @@
+#ifndef _TEXTINPUTSCREEN_H_
+#define _TEXTINPUTSCREEN_H_
+
+#include "SSD1306.h"
+#include "Screen.h"
+#include <Arduino.h>
+
+// the first char in ascii that is not a control char (space)
+#define FIRST_CHAR 0x20
+
+// the last char in ascii that is a valid character
+#define LAST_CHAR 0x7E
+#define START_CHAR 0x41
+
+class TextInputScreen : public Screen {
+private:
+  int mProgress;
+  const String mMessage;
+  String mInput;
+  int mCurrentCharPos;
+
+public:
+  TextInputScreen(SSD1306 *display, const String message);
+  ~TextInputScreen();
+  void draw();
+
+  inline String getInputString() { return mInput; }
+
+  inline void nextChar() {
+    // duplicate the last char, that makes the input easier
+    // because if you enter a long numeric password
+    // or a sequence of special chars you're already
+    // in the right region of the ascii table
+    char lastChar = mInput.charAt(mCurrentCharPos);
+    mInput += lastChar;
+    mCurrentCharPos++;
+  }
+
+  inline void prevChar() {
+    if (mInput.length() > 1) {
+      mInput.remove(mInput.length() - 1);
+      mCurrentCharPos--;
+    }
+  }
+
+  inline void charUp() {
+    char current = mInput.charAt(mCurrentCharPos);
+    char newChar = current + 1 > LAST_CHAR ? FIRST_CHAR : current + 1;
+    mInput.setCharAt(mCurrentCharPos, newChar);
+  }
+
+  inline void charDown() {
+    char current = mInput.charAt(mCurrentCharPos);
+    char newChar = current - 1 < FIRST_CHAR ? LAST_CHAR : current - 1;
+    mInput.setCharAt(mCurrentCharPos, newChar);
+  }
+};
+
+#endif //_TEXTINPUTSCREEN_H_
